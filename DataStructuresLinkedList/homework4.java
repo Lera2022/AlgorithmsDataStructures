f206d9f012162941ff5c5ef7079a79ebc079b5e0
Lera2022 <101706797+Lera2022@users.noreply.github.com>
2023-04-17 20:13:58 +0500

    homework 4 modified

--- DataStructuresLinkedList/homework4.java
+++ DataStructuresLinkedList/homework4.java
@@ -1,111 +1,143 @@
-import java.util.Iterator;
+public void rotateLeft() {
+  if (head != null) {                       //otherwise it is empty list
+    Node temp = head;
+    if (head.getNext() != null) {           //otherwise it is single item list
+        head = head.getNext();
+    }
 
-public class Main {
+    Node tail;
+    if (head.getNext() != null) {
+        //more than 2 items in the list
+        tail = head.getNext();
+    } else {
+        //only 2 items in the list
+        tail = head;
+    }
+    while (tail.getNext() != null) {
+        if (tail.getNext() != null) {
+            tail = tail.getNext();
+        }
+    }
+    tail.setNext(temp);
+    temp.setNext(null);
+  }
+}
+
+public void rotateRight() {
+  if (head != null) {                       //otherwise it is empty list
+    Node tail = null;
+    Node current = head;
+
+    while (current.getNext() != null) {
+        tail = current;
+        current = current.getNext();
+    }
+    if (tail != null) {             //otherwise it is single item list
+        tail.setNext(null);
+        current.setNext(head);
+        head = current;
+    }
+  }
+}
+
+public class LinkedList {
+
+  Node head;
+
+  public LinkedList() {
+      this.head = null;
+  }
+
+  public LinkedList(Node head) {
+      this.head = head;
+  }
+
+  public void rotateLeft() {
+      System.out.println("<-");
+      if (head != null) { // otherwise it is empty list
+          Node temp = head;
+          if (head.getNext() != null) { // otherwise it is single item list
+              head = head.getNext();
+          }
+
+          Node tail;
+          if (head.getNext() != null) {
+              // more than 2 items in the list
+              tail = head.getNext();
+          } else {
+              // only 2 items in the list
+              tail = head;
+          }
+          while (tail.getNext() != null) {
+              if (tail.getNext() != null) {
+                  tail = tail.getNext();
+              }
+          }
+          tail.setNext(temp);
+          temp.setNext(null);
+      }
+  }
+
+  public void rotateRight() {
+      System.out.println("->");
+      if (head != null) { // otherwise it is empty list
+          Node tail = null;
+          Node current = head;
+
+          while (current.getNext() != null) {
+              tail = current;
+              current = current.getNext();
+          }
+          if (tail != null) { // otherwise it is single item list
+              tail.setNext(null);
+              current.setNext(head);
+              head = current;
+          }
+      }
+  }
+
+  public void printList() {
+      Node cursor = head;
+      while (cursor != null) {
+          System.out.print(cursor.data + ", ");
+          cursor = cursor.getNext();
+      }
+      System.out.println();
+  }
+
+  public void add(Object data) {
+      Node temp = new Node(data);
+
+      if (head == null) {
+          head = temp;
+      } else {
+          Node current = head;
+
+          while (current.getNext() != null) {
+              current = current.getNext();
+          }
+
+          current.setNext(temp);
+      }
+  }
 
   public static void main(String[] args) {
-    SingleLinkList<Contact> contactList = new SingleLinkList<>();
-
-    contactList.addToEnd(new Contact(121, "Иванов Иван Иванович", "+7987654321"));
-    contactList.addToEnd(new Contact(122, "Иванов Сергей Иванович", "+7987654322"));
-    contactList.addToEnd(new Contact(123, "Иванов Андрей Иванович", "+7987654323"));
-    contactList.addToEnd(new Contact(124, "Иванов Тимофей Иванович", "+7987654324"));
-    contactList.addToEnd(new Contact(125, "Иванов Александр Иванович", "+7987654325"));
-
-    for (Object contact : contactList) {
-      System.out.println(contact);
-    }
-    contactList.reverse();
-
-    System.out.println("-------------------------------------");
-
-    for (Object contact : contactList) {
-      System.out.println(contact);
-    }
+      LinkedList r = new LinkedList();
+      r.add(1);
+      r.add(2);
+      r.add(3);
+      r.add(4);
+      r.add(4);
+      r.add(5);
+      r.printList();
+      r.rotateLeft();
+      r.printList();
+      r.rotateLeft();
+      r.printList();
+      r.rotateRight();
+      r.printList();
+      r.rotateRight();
+      r.printList();
   }
 
-  static class Contact {
-
-    int id;
-    String name;
-    String phone;
-
-    public Contact(int id, String name, String phone) {
-      this.id = id;
-      this.name = name;
-      this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-      return "Contact{" +
-          "id=" + id +
-          ", name='" + name + '\'' +
-          ", phone='" + phone + '\'' +
-          '}';
-    }
-  }
-
-  public static class SingleLinkList<T> implements Iterable {
-
-    ListItem<T> head;
-    ListItem<T> tail;
-
-    @Override
-    public Iterator iterator() {
-      return new Iterator<T>() {
-        ListItem<T> current = head;
-
-        @Override
-        public boolean hasNext() {
-          return current != null;
-        }
-
-        @Override
-        public T next() {
-          T data = current.data;
-          current = current.next;
-          return data;
-        }
-      };
-    }
-
-
-    private static class ListItem<T> {
-
-      T data;
-      ListItem<T> next;
-    }
-
-    public boolean isEmpty() {
-      return head == null;
-    }
-
-    public void addToEnd(T item) {
-
-      ListItem<T> newItem = new ListItem<>();
-      newItem.data = item;
-
-      if (isEmpty()) {
-        head = newItem;
-        tail = newItem;
-      } else {
-        tail.next = newItem;
-        tail = newItem;
-      }
-    }
-
-    public void reverse() {
-      if (!isEmpty() && head.next != null) {
-        tail = head;
-        ListItem<T> current = head.next;
-        head.next = null;
-        while (current != null) {
-          ListItem<T> next = current.next;
-          current.next = head;
-          head = current;
-          current = next;
-        }
-      }
-    }
-  }
 }
